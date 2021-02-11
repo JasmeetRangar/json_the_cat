352bf0b0fd7a05c636d63bc188f3fbeec71f83e7
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-11 19:58:28 +0000

    test files added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,22 @@
 const request = require('request');
-if (process.argv.length < 3) {
-  console.log('not enough parameters!');
-  process.exit();
-}
-
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
-const requestCallback = (error, response, body) => {
-  if (!response || response.statusCode !== 200) {
-    console.log('Request Error:', response && response.statusCode);
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log("Not a valid breed. try again please");
-  } else {
-    console.log(data[0].description);
-  }
+// if (process.argv.length < 3) {
+//   console.log('not enough parameters!');
+//   process.exit();
+// }
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  const requestCallback = (error, response, body) => {
+    if (!response || response.statusCode !== 200) {
+      return callback(" " + response + response.statusCode, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      return callback("Invalid breed", null);
+    } else {
+      return callback(null, data[0].description);
+    }
+  };
+  request(url, requestCallback);
 };
-request(url, requestCallback);
+
+module.exports = {fetchBreedDescription};
